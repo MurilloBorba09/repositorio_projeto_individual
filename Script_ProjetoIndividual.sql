59e47b616c9623d9b1e6108dd414376577cd7956
MurilloBorba09 <murillo.borba@sptech.school>
2022-06-01 15:51:43 -0300

    Arrumando CSS geral e adicionando materia

--- Script_ProjetoIndividual.sql
+++ Script_ProjetoIndividual.sql
@@ -2,6 +2,8 @@ create database projeto_individual;
 
 use projeto_individual;
 
+-- drop database projeto_individual;
+
 create table usuario (
 	idUsuario int primary key auto_increment,
     email varchar (90),
@@ -18,6 +20,29 @@ desc usuario;
 
 insert into usuario values 
 	(null, '', '', '', '', '', '', '', ''),
+	(null, '', '', '', '', '', '', '', ''),
     (null, '', '', '', '', '', '', '', '');
     
 select * from usuario;
+
+create table nota (
+	idNota int auto_increment,
+	notaSite varchar (2),
+	opiniaoSite varchar (500),
+	notaMateria varchar (2),
+	fkUsuario int,
+	foreign key (fkUsuario) references usuario(idUsuario)
+	primary key (idNota, fkUsuario)
+);
+
+desc usuario;
+
+insert into nota values 
+	(null, '', '', '', ),
+	(null, '', '', '', ),
+	(null, '', '', '', ),
+	(null, '', '', '', ),
+	(null, '', '', '', ),
+	(null, '', '', '', );
+
+select * from nota;
